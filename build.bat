0b7d0e788b2aa26a2fae6ba6f4aad1a64791b09b
JukeLiv <soniccrackgames52@gmail.com>
2023-07-05 19:08:08 -0400

    Adding a logo to Kaite
    the animal in the logo is a KodKod, this animal can be found in Chile ( my country ) and Argentina. And it is really cute

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
 REM this is ugly, but we need to build Kaite in some way while it is runing :(
-gcc src\utils.c src\lexer.c src\parser.c src\interpreter.c src\main.c -o bin\kaite.exe -O2
+
+windres res\kaite.rc -O coff -o res\kaite.res
+gcc src\utils.c src\lexer.c src\parser.c src\interpreter.c src\main.c res\kaite.res -o bin\kaite.exe -O2
