0e829d0685b1f82318a4d406bc8a111ec5a9343e
Xenon <xenon.log01@gmail.com>
2023-08-12 22:25:20 -0700

    general updates

--- src/parser.c
+++ src/parser.c
@@ -101,7 +101,7 @@ Expr Expr_Literal(Token tok)
 
         default:
         {
-            printf("ERROR: Can't set Literal to Token Type!!! (%d : %s )\n", tok.type, tok.str.content);
+            printf("ERROR: Token is not a literal!!! (%d : %s)\n", tok.type, tok.str.content);
             exit(1);
         }
         break;
